69544c689b736f917ab2bc31fa2b46c49c9560b4
haltosan <ethanhsimmons@gmail.com>
2021-09-07 10:30:41 -0600

    Cleaned up predicates.py

--- predicates.py
+++ predicates.py
@@ -12,31 +12,39 @@ SHORT_LEN = 5
 MOSTLY_CAPS_THRESHOLD = .75
 REGULAR_EXPRESSION = r'^(?P<name>[A-Z][a-z]+, ([A-Z][a-z]+ ?)+).*'
 
+
 def lower(text):
     return text.islower()
-    
+
+
 def upper(text):
     return text.isupper()
-    
+
+
 def blank(text):
     return len(text) == 0
+
 
 def alpha(text):
     return text.isalpha()
 
+
 def notAlpha(text):
     return not text.isalpha()
 
+
 def alphaSpace(text):
     for char in text:
-        if(char.isalpha() or char == ' '):
+        if char.isalpha() or char == ' ':
             pass
         else:
             return False
     return True
 
+
 def alphaSpaceChar(text):
     return text.isalpha() or text == ' '
+
 
 def upperWord(text):
     try:
@@ -44,56 +52,71 @@ def upperWord(text):
     except IndexError:
         return False
 
-def false(text):
-    return False
 
 def hasPage(text):
     return "PAGE" in text
 
+
 def name(text):
     for char in text:
-        if(char.isalpha() or char == ' ' or char == ','):
+        if char.isalpha() or char == ' ' or char == ',':
             pass
         else:
             return False
     return True
+
 
 def nameChar(text):
     return text.isalpha() or text == ' ' or text == ','
 
+
 def address(text):
     for char in text:
-        if(char.isalpha() or char.isdigit() or char == ' ' or char == ',' or char == ','):
+        if char.isalpha() or char.isdigit() or char == ' ' or char == ',' or char == ',':
             pass
         else:
             return False
     return True
 
+
 def addressChar(text):
     return text.isalpha() or text.isdigit() or text == ' ' or text == ',' or text == '.'
+
 
 def short(text):
     return len(text) <= SHORT_LEN
 
+
 def long(text):
     return not short(text)
 
+
 def mostlyCaps(text):
-    text = text.replace(' ','')
-    if(len(text) == 0):
+    text = text.replace(' ', '')
+    if len(text) == 0:
         return False
     score = 0
     for char in text:
-        if(char.isupper()):
+        if char.isupper():
             score += 1
     return (score / len(text)) >= MOSTLY_CAPS_THRESHOLD
 
+
 def indented(text):
-    if(len(text) == 0):
+    if len(text) == 0:
         return False
     return text[0] == ' '
+
 
 def regex(text):
     expression = re.compile(REGULAR_EXPRESSION)
     result = expression.search(text)
-    return not result is None #result is None when re doesn't find anything
+    return result is not None  # result is None when re doesn't find anything
+
+
+def printableChar(text):
+    return (ord(text) >= 32) and (ord(text) <= 126)  # ascii numbers for printable chars
+
+
+def space(text):
+    return text.isspace()
