abf45b6858828b59c6b67325899a01df690b998b
haltosan <32181538+haltosan@users.noreply.github.com>
2022-03-04 12:19:24 -0700

    Add files via upload

--- dataMatching.py
+++ dataMatching.py
@@ -1,16 +1,14 @@
 from file_analysis import *
 
 
-def getColumn(csvFile: list, key: str) -> list:
+def getColumn(table: list, key: str) -> list:
     """Get a column from a csv file (as produced by file_analysis.get(fname))"""
-    i = 0
-    while True:
-        col = csvColumn(csvFile, i)
-        if col[0] == key:  # checks the first value of the row to see if it matches the column key exactly
-            return col[1:]
-        if i > 1000:  # some arbitrary value to indicate we haven't reached a column with that value because csvColumn will always return something
-            return None
-        i += 1
+    keys = tableKeys(table)
+    try:
+        i = keys.index(key)
+        return csvColumn(table, i)[1:]  # the first value is the key value
+    except ValueError:
+        return None
 
 def matchOnValue(tableA: list, tableB: list, columnKey: str) -> tuple:
     """Generator. Returns the rows that match in some column in both csv files (file_analysis.get('foo.csv')) in the form (rowA, rowB)"""
@@ -22,13 +20,77 @@ def matchOnValue(tableA: list, tableB: list, columnKey: str) -> tuple:
                 yield tableA[aIndex + 1], tableB[bIndex + 1]  # 1 is added because the getColumn functions remove the first value (column key) when they return
 
 def fuzzyMatch(tableA: list, tableB: list, columnKey: str, matchFunction) -> tuple:
-    """Generator. Same as match on value but uses fuzzyFunction to determine if 2 values match"""
+    """Generator. Same as match on value but uses matchFunction to determine if 2 values match"""
     aColumn = getColumn(tableA, columnKey)
     bColumn = getColumn(tableB, columnKey)
     for aIndex in range(len(aColumn)):
         for bIndex in range(len(bColumn)):
             if matchFunction(aColumn[aIndex], bColumn[bIndex]) == True:
                 yield tableA[aIndex + 1], tableB[bIndex + 1]
+
+def join(tableA: list, tableB: list, columnKey: str, matchFunction = lambda x,y : x == y, reason: str = None) -> list:
+    """Combine 2 tables if they match on some column value"""
+    matches = []
+    matches.append(csvJoin( tableKeys(tableA) + tableKeys(tableB)))
+    for match in fuzzyMatch(tableA, tableB, columnKey, matchFunction):
+        matches.append(match[0] + ',' + match[1])
+    return matches
+    
+
+def tableKeys(table: list) -> list:
+    """Returns the key values from a csv file"""
+    return csvSplit(table[0])
+
+def project(table: list, newHeader: list) -> list:
+    """Change column ordering to match the new header"""
+    transpose = []
+    for key in newHeader:
+        transpose.append(getColumn(table, key))  # add the columns of interest as a row
+    
+    outTable = []
+    outTable.append(csvJoin(newHeader))  # set the header
+    for rowIndex in range(len(transpose[0])):  # rotate the table
+        row = []
+        for columnIndex in range(len(transpose)):
+            row.append(transpose[columnIndex][rowIndex])
+        outTable.append(csvJoin(row))
+    return outTable
+
+def projectNumerical(table: list, newHeader: list) -> list:
+    """Project using a numerical header of column numbers"""
+    transpose = []
+    for i in newHeader:
+        transpose.append(csvColumn(table, i)[1:])  # exclude the key value from the column
+
+    # calculate the new header
+    outTable = []
+    oldHeader = tableKeys(table)
+    newHeaderString = []
+    for i in newHeader:
+        newHeaderString.append(oldHeader[i])
+    newHeaderString = csvJoin(newHeaderString)
+    outTable.append(newHeaderString)
+
+    for rowIndex in range(len(transpose[0])):
+        row = []
+        for columnIndex in range(len(transpose)):
+            row.append(transpose[columnIndex][rowIndex])
+        outTable.append(csvJoin(row))
+    return outTable
+
+def matchingColumns(table: list, column1: int, column2: int) -> list:
+    """Generator. Filters rows where table[column1] == table[column2]"""
+    for row in table:
+        splitRow = csvSplit(row)
+        if splitRow[column1] == splitRow[column2]:
+            yield row
+
+def getAll(generator) -> list:
+    """Return all generated values from a given generator"""
+    output = []
+    for item in generator:
+        output.append(item)
+    return out
 
 '''
 >>> a = get('a.csv')
