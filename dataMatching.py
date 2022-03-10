cce5a389c40402dadca4189c63e3a342e8a70ab1
haltosan <32181538+haltosan@users.noreply.github.com>
2022-03-10 13:57:39 -0700

    Add files via upload

--- dataMatching.py
+++ dataMatching.py
@@ -1,5 +1,71 @@
 from file_analysis import *
+from pandas import to_datetime
+from datetime import *
 
+x = open('test_obits.csv')
+red = x.read()
+x.close()
+obits = red.split('\n')
+del red
+db = get('test_database.csv')
+okey = {'Image':0, 'First_Names':1, 'Last_Name':2, 'Death_Date_Day':3, 'Death_Date_Month':4, 'Death_Date_Year':5,
+        'Birth_Date_Day':6, 'Birth_Date_Month':7, 'Birth_Date_Year':8, 'Names':9, 'Dates':10, 'Locations':11,
+        'Surname_1':12, 'Surname_2':13, 'Surname_3':14, 'First_1':15, 'First_2':16, 'First_3':17}
+dkey = {'':0, 'birthlikedate':1, 'deathlikedate':2, 'surname':3, 'givenname':4, 'deathlikeplace':5,
+        'birthlikeplace':6, 'sex':7, 'fs_url':8, 'firstname':9}
+def o(line, key):
+    return csvSplit(line)[okey[key]]
+def d(line, key):
+    return csvSplit(line)[dkey[key]]
+
+def dYear(dline, oline):
+    dY = to_datetime(d(dline, 'deathlikedate')).year
+    oY = int(o(oline, 'Death_Date_Year'))
+    return dY==oY
+def bYear(dline, oline):
+    dY = to_datetime(d(dline, 'birthlikedate')).year
+    oY = int(o(oline, 'Birth_Date_Year'))
+    return dY==oY
+def surname(dline, oline):
+    dS = d(dline, 'surname')
+    oS = o(oline, 'Last_Name')
+    return dS.lower() == oS.lower()
+def twoPlus(dline, oline):
+    score = 0
+    if surname(dline, oline):
+            score += 1
+    if dYear(dline, oline):
+            score += 1
+    if bYear(dline, oline):
+            score += 1
+    return score >= 2
+
+def anySur(dline, oline):
+    oS = [o(oline, 'Last_Name').lower(), o(oline, 'Surname_1').lower(), o(oline, 'Surname_2').lower(), o(oline, 'Surname_3').lower()]
+    dS = d(dline, 'surname')
+    return dS.lower() in oS
+def anyGiven(dline, oline):
+    dG = d(dline, 'givenname')
+    oG = [o(oline, 'First_Names').lower(), o(oline, 'First_1').lower(), o(oline, 'First_2').lower(), o(oline, 'First_3').lower()]
+    return dG.lower() in oG
+def twoPlusAnyName(dline, oline):
+    score = 0
+    if anySur(dline, oline):
+        score += 1
+    if anyGiven(dline, oline):
+        score += 1
+    if dYear(dline, oline):
+            score += 1
+    if bYear(dline, oline):
+            score += 1
+    return score >= 2
+
+def leftovers(old, output):
+    idSet = set(getColumn(old, 'id'))
+    outID = getColumn(output, 'id')
+    for i in range(len(output)):
+        if outID[i] not in idSet:
+            yield output[i]
 
 def getColumn(table: list, key: str) -> list:
     """Get a column from a csv file (as produced by file_analysis.get(fname))"""
@@ -21,9 +87,20 @@ def matchOnValue(tableA: list, tableB: list, columnKey: str) -> tuple:
 
 def fuzzyMatch(tableA: list, tableB: list, columnKey: str, matchFunction) -> tuple:
     """Generator. Same as match on value but uses matchFunction to determine if 2 values match"""
-    aColumn = getColumn(tableA, columnKey)
-    bColumn = getColumn(tableB, columnKey)
+    if columnKey == None:
+        aColumn = tableA[1:]  # uses entire rows, but removes the header/keys
+        bColumn = tableB[1:]
+    else:
+        aColumn = getColumn(tableA, columnKey)
+        bColumn = getColumn(tableB, columnKey)
+    outerLen = len(aColumn)
     for aIndex in range(len(aColumn)):
+        if aIndex == int(outerLen * .25):
+            print('1/4 done')
+        elif aIndex == int(outerLen * .5):
+            print('halfway done')
+        elif aIndex == int(outerLen * .75):
+            print('3/4 done')
         for bIndex in range(len(bColumn)):
             if matchFunction(aColumn[aIndex], bColumn[bIndex]) == True:
                 yield tableA[aIndex + 1], tableB[bIndex + 1]
@@ -101,7 +178,7 @@ def getAll(generator) -> list:
 '''
 >>> a = get('a.csv')
 >>> b = get('b.csv')
->>> i = fuzzyMatch(a, b, 'date', lambda x, y: (int(x) - int(y)) < 2)
+>>> i = fuzzyMatch(a, b, 'date', lambda x, y: abs(int(x) - int(y)) < 2)
 >>> for x in i:
 	print(x)
 
