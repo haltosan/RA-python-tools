fc76bd8d00beb7a20251e61af2a4dfe27403b611
haltosan <32181538+haltosan@users.noreply.github.com>
2022-03-31 12:46:28 -0600

    Add files via upload

--- dataMatching.py
+++ dataMatching.py
@@ -2,6 +2,7 @@ from file_analysis import *
 from pandas import to_datetime
 from datetime import *
 
+'''
 x = open('obit_output-filtered-twoPlus-surname.csv', 'r', encoding='utf-8')
 red = x.read()
 x.close()
@@ -10,6 +11,8 @@ del obits[-1]
 del red
 db = get('database with death year.csv')
 del db[-1]  # last cell is blank
+'''
+
 okey = {'Image':0, 'First_Names':1, 'Last_Name':2, 'Death_Date_Day':3, 'Death_Date_Month':4, 'Death_Date_Year':5,
         'Birth_Date_Day':6, 'Birth_Date_Month':7, 'Birth_Date_Year':8, 'Names':9, 'Dates':10, 'Locations':11,
         'Surname_1':12, 'Surname_2':13, 'Surname_3':14, 'First_1':15, 'First_2':16, 'First_3':17}
@@ -234,10 +237,23 @@ Another use is looking up rows by value. If key=id and data=entireTable, dict[5]
     out = dict()
     for i in range(len(keyCol)):
         key = keyCol[i].lower()
-        data = dataCol[i].lower()
+        data = dataCol[i]
         if key not in out:
             out[key] = set()
         out[key].add(data)
+    return out
+
+def buildDictList(keyCol: list, dataCol: list) -> dict:
+    """Creates a dictionary that has a key column value correspond to some data column value.
+For example, if key=name and data=id, dict['Bob'] would return a set of id's that have the name Bob.
+Another use is looking up rows by value. If key=id and data=entireTable, dict[5] would be the rows that have an id of 5"""
+    out = dict()
+    for i in range(len(keyCol)):
+        key = keyCol[i].lower()
+        data = dataCol[i]
+        if key not in out:
+            out[key] = list()
+        out[key].append(data)
     return out
 
 def dictMatch(tableA: list, tableB: list, keyA: str, keyB: str, header: list, matchFunction = lambda dbLine, obitsLine: dbLine == obitsLine) -> list:
@@ -380,9 +396,128 @@ def dictResult(r: list, h: list) -> list:
     return out
 
 
+skagitKey = {'FamilySearch':0,'Firstname':1,'Surname':2,'Spouse Surname':3,'Death date':4,'Birth date':5,'Birth place':6,
+      'Death place':7,'Burial Place':8,'Kidsnames':9}
+getSkagit = lambda row, key: csvSplit(row)[skagitKey[key]]
+dbKey = {'pid':0,'id':1,'url':2,'image':3,'reason':4,'id':5,'birthlikedate':6,'deathlikedate':7,'surname':8,
+      'givenname':9,'deathlikeplace':10,'birthlikeplace':11,'sex':12,'url':13,'firstname':14}
+getDb = lambda row, key: csvSplit(row)[dbKey[key]]
 
+def scoreSkagit(rowA, rowB) -> int:
+    score = 0
+    #if fname matches
+    if getSkagit(rowA,'Firstname').lower() == getDb(rowB,'firstname').lower():
+        score += 1
+    #if surname matches
+    if getSkagit(rowA,'Surname').lower() == getDb(rowB,'surname').lower():
+        score += 1
+    #if death date matches
+    if getSkagit(rowA,'Death date').lower() == getDb(rowB,'deathlikedate').lower():
+        score += 1
+    #if birth matches
+    if getSkagit(rowA,'Birth date').lower() == getDb(rowB,'birthlikedate').lower():
+        score += 1
+    return score
 
+# a = skagit; b = db
+def dictSkagitMatch(tableA: list, tableB: list) -> list:
+    out = list()
+    out.append('GIMME HEADER')
+    
+    pidDict = buildDict(getColumn(tableA, 'FamilySearch'), tableA[1:])
+    surnameDict = buildDict(getColumn(tableA, 'Surname'), tableA[1:])
+    bPID = getColumn(tableB, 'pid')
+    bReason = getColumn(tableB, 'reason')
+    bImage = getColumn(tableB, 'image')
+    bSurname = getColumn(tableB, 'surname')
+    tableB = tableB[1:]
+    mx = len(tableB)
+    for i in range(len(tableB)):
+        pid = bPID[i]
+        surname = bSurname[i]
+        image = bImage[i]
+        reason = bReason[i]
+        if pid in pidDict:
+            lines = pidDict[pid]
+            if len(lines) == 1:
+                rowA = list(lines)[0]
+                out.append(rowA + ',' + image + ',' + reason)
+                continue
+        
+        if surname in surnameDict:
+            lines = surnameDict[surname]
+            if len(lines) == 1:
+                rowA = list(lines)[0]
+                out.append(rowA + ',' + image + ',' + reason)
+            else:
+                bestLine = ''
+                maxLineScore = -1
+                for line in lines:
+                    lineScore = scoreSkagit(line, tableB[i])
+                    if lineScore > maxLineScore:
+                        maxLineScore = lineScore
+                        bestLine = line
+                out.append(bestLine + ',' + image + ',' + reason)
+        
+        if i == int(mx * .1):
+            print('10% done')
+        elif i == int(mx * .25):
+            print('25% done')
+        elif i == int(mx * .5):
+            print('50% done')
+        elif i == int(mx * .75):
+            print('75% done')
+    return out
 
+def scoreReason(rString):
+    if rString == '2+ matched from surname/death year/birth year':
+        return 5
+    if rString == 'first name and surname matched':
+        return 4
+    if rString == 'matching birth and death':
+        return 3
+    if rString == 'surname' or rString == 'first name' or rString == 'first name or surname matched':
+        return 2
+    return 1
+
+def dictSVGS(sv, db):
+    out = list()
+    out.append('GIMME HEADER')
+
+    pidToImage = buildDictList(getColumn(db,'pid'), getColumn(db, 'image'))
+    pidToReason = buildDictList(getColumn(db, 'pid'), getColumn(db, 'reason'))
+    sPid = getColumn(sv, 'pid')
+    sv = sv[1:]
+    mx = len(sv)
+    for i in range(len(sPid)):
+        sLine = sv[i]
+        pid = sPid[i].lower()
+        image = ''
+        reason = ''
+        if len(pid) < 5:
+            pass
+        elif pid in pidToImage and pid in pidToReason:
+            images = pidToImage[pid]
+            reasons = pidToReason[pid]
+            bestReason = -1
+            for r in range(len(reasons)):
+                s = scoreReason(reasons[r])
+                if s > bestReason:
+                    bestReason = s
+                    reason = reasons[r]
+                    image = images[r]
+
+        out.append(sLine + ',' + image + ',' + reason)
+        if i == int(mx * .1):
+            print('10% done')
+        elif i == int(mx * .25):
+            print('25% done')
+        elif i == int(mx * .5):
+            print('50% done')
+        elif i == int(mx * .75):
+            print('75% done')
+    
+    return out
 
 
 
